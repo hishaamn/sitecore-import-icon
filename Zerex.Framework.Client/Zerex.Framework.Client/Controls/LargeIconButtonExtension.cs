6cae73a45914561630fb31e502247ebafb4440bb
Hishaam <hishaam.namooya@proximitybbdo.fr>
2020-12-04 18:11:57 +0400

    Code Refactoring

--- Zerex.Framework.Client/Zerex.Framework.Client/Controls/LargeIconButtonExtension.cs
+++ Zerex.Framework.Client/Zerex.Framework.Client/Controls/LargeIconButtonExtension.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.UI;
+﻿using System.Web.UI;
 using Sitecore;
 using Sitecore.Resources;
 using Sitecore.Shell.Web.UI.WebControls;
@@ -16,34 +11,47 @@ namespace Zerex.Framework.Client.Controls
         protected override void Render(HtmlTextWriter output)
         {
             if (!this.Visible)
+            {
                 return;
-            string str1 = StringUtil.GetString(this.ToolTip);
-            string str2 = str1.Length > 0 ? " title=\"" + str1 + "\"" : string.Empty;
+            }
+
+            var tooltip = StringUtil.GetString(this.ToolTip);
+
+            var title = tooltip.Length > 0 ? " title=\"" + tooltip + "\"" : string.Empty;
+
             if (!this.Enabled)
             {
-                output.Write("<span class=\"scRibbonToolbarLargeIconButtonDisabled\"" + str2 + ">");
+                output.Write("<span class=\"scRibbonToolbarLargeIconButtonDisabled\"" + title + ">");
+
                 output.Write("<span class=\"scRibbonToolbarLargeIconButtonIconGrayed\">");
+
                 output.Write(new ImageBuilder()
                 {
                     Src = Images.GetThemedImageSource(this.Icon, ImageDimension.id32x32),
                     Disabled = true,
                     Class = "scRibbonToolbarLargeIconButtonIconDisabled",
-                    Alt = str1
+                    Alt = tooltip
                 }.ToString());
+
                 output.Write("</span>");
+
                 output.Write("</span>");
             }
             else
             {
-                string str3 = this.ID == null || this.ID.Length <= 0 ? string.Empty : " id=\"" + this.ID + "\"";
-                string str4 = this.AccessKey == null || this.AccessKey.Length <= 0 ? string.Empty : " accesskey=\"" + this.AccessKey + "\"";
-                output.Write("<a" + str3 + " href=\"#\" class=\"scRibbonToolbarLargeIconButton" + (this.Down ? "Down" : string.Empty) + "\"" + str2 + str4 + " onclick=\"javascript:return scForm.invoke('" + this.Command + "')\">");
+                var id = this.ID == null || this.ID.Length <= 0 ? string.Empty : " id=\"" + this.ID + "\"";
+
+                var accessKey = this.AccessKey == null || this.AccessKey.Length <= 0 ? string.Empty : " accesskey=\"" + this.AccessKey + "\"";
+
+                output.Write("<a" + id + " href=\"#\" class=\"scRibbonToolbarLargeIconButton" + (this.Down ? "Down" : string.Empty) + "\"" + title + accessKey + " onclick=\"javascript:return scForm.invoke('" + this.Command + "')\">");
+
                 output.Write(new ImageBuilder()
                 {
                     Src = Images.GetThemedImageSource(this.Icon, ImageDimension.id32x32),
                     Class = "scRibbonToolbarLargeIconButtonIcon",
-                    Alt = str1
+                    Alt = tooltip
                 }.ToString());
+
                 output.Write("</a>");
             }
         }
